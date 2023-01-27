f65f17530fb7606cc28a0ea57b5a5f5bc21ee6ff
ATVQ (Attila Vural) <ATVQ@novonordisk.com>
2023-01-27 17:06:04 +0100

    Ability to search custom string added. Ability to save current search added

--- no_flask_main.py
+++ no_flask_main.py
@@ -3,56 +3,110 @@ from bokeh.plotting import figure, curdoc
 from bokeh.models import DatetimeTickFormatter, RELATIVE_DATETIME_CONTEXT
 import snscrape_test as sn
 import pandas as pd
-    
-p = figure(height=350, width=1500, x_axis_type="datetime")
-
-df = sn.sentiment("brøndby if", 100)
-###########
+from bokeh.models import Button, TextInput, ColumnDataSource
+from bokeh.layouts import column
 import pymongo
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 dblist = myclient.list_database_names()
 mydb = myclient["mydatabase"]
 mycol = mydb["records"]
-x = mycol.insert_many(df.to_dict(orient='records'))
-# for x in mycol.find():
-    # print(x)
-# mycol.delete_many({'name': 'Peter'})
+from datetime import datetime, timedelta
 
-    
-################
-# becomes a pandas series type
-#df = df.groupby(df.date.dt.day)['com_score'].mean()
-df = df.groupby(df.date.dt.to_period('D'))['com_score'].mean()
 
-# Convert back to a pandas dataframe
-df = pd.DataFrame({'date':df.index, 'com_score':df.values})
-
-print(df)
+p = figure(height=350, width=1500, x_axis_type="datetime")
 
 p.circle(
-    df['date'],
-    df['com_score'],
+    [],
+    [],
     size=20,
     color="navy",
     alpha=0.5
 )
 p.line(
-    df['date'],
-    df['com_score']
+    [],
+    []
 )
 
 # Format the axis to show dates properly
 p.xaxis.formatter.context = RELATIVE_DATETIME_CONTEXT()
-# p.xaxis.formatter = DatetimeTickFormatter(years="%d/%m/%Y %H:%M:%S",
-                                          # months="%d/%m/%Y %H:%M:%S",
-                                          # days="%d/%m/%Y %H:%M:%S",
-                                          # hours="%d/%m/%Y %H:%M:%S",
-                                          # hourmin="%d/%m/%Y %H:%M:%S",
-                                          # minutes="%d/%m/%Y %H:%M:%S",
-                                          # minsec="%d/%m/%Y %H:%M:%S",
-                                          # seconds="%d/%m/%Y %H:%M:%S",
-                                          # milliseconds="%d/%m/%Y %H:%M:%S",
-                                          # microseconds="%d/%m/%Y %H:%M:%S")
+
+# create a callback that saves the data
+def save_records():
+    document_count = mycol.count_documents({})
+    print("Documents before insertion", document_count)
+    x = mycol.insert_many(raw_df.to_dict(orient='records'))
+    document_count = mycol.count_documents({})
+    print("Documents after insertion", document_count)
+    
+    # print("The following data has been saved:")
+    # for x in mycol.find():
+        # print(x)
+    #mycol.delete_many({'name': 'Peter'})
+    
+def get_records():
+    # Remove previous glyphs
+    p.renderers = []
+    # Scrape data
+    
+    ###################################################
+    ########## Eksperimentiel kode ####################
+    ###################################################
+    # Virker pt som den originale kode, men når du fjerne udkommenteringen ved "search_string" giver den fejl
+    yesterday = datetime.now() - timedelta(10)  
+    search_string = text_input.value# + " since:" + datetime.strftime(yesterday, '%Y-%m-%d') + " until:" + datetime.strftime(yesterday, '%Y-%m-%d')
+    print(search_string)
+    raw_df = sn.sentiment(search_string, 100)
+    print(raw_df)
+    # for i in range(30):
+        # yesterday = datetime.now() - timedelta(i)  
+        # search_string = text_input.value + "since:" + datetime.strftime(yesterday, '%Y-%m-%d')
+        # print(search_string)
+        # temp = sn.sentiment(search_string, 100)
+        # pd.concat([raw_df, temp])
+        
+    ################################################### 
+    ###################################################
+    ################################################### 
+    
+    #############################################################
+    ######### Udkommenteret original kode: ######################
+    #############################################################
+    # raw_df = sn.sentiment(text_input.value, 100)
+    #############################################################
+    #############################################################
+    #############################################################
+    
+    # Group by date: day. This converts it from a dataframe to a series
+    df = raw_df.groupby(raw_df.date.dt.to_period('D'))['com_score'].mean()
+    # Convert back to a pandas dataframe
+    df = pd.DataFrame({'date':df.index, 'com_score':df.values})
+    
+    print("Input field value:", text_input.value)
+    print("df.size: ", df.size)
+    print("len():", len(df))
+    print(df)
+
+    p.circle(
+        df['date'],
+        df['com_score'],
+        size=20,
+        color="navy",
+        alpha=0.5
+    )
+    p.line(
+        df['date'],
+        df['com_score']
+    )
+    
+
+# add a button widget and configure with the call back
+save_button = Button(label="Save data")
+save_button.on_event('button_click', save_records)
+
+search_button = Button(label="Get data")
+search_button.on_event('button_click', get_records)
+
+text_input = TextInput(value="default", title="Label:")
 
 # put the button and plot in a layout and add to the document
-curdoc().add_root(p)
+curdoc().add_root(column(save_button, search_button, text_input, p))
