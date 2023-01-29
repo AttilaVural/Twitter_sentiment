795debec7f19055e2e1371e1aa130a35d510f91a
mulvarp334 <att.vural@gmail.com>
2023-01-29 23:28:47 +0100

    made since:... and until:.. work with snscraper. Added more textfields. Reformatted code a bit

--- no_flask_main.py
+++ no_flask_main.py
@@ -3,29 +3,26 @@ from bokeh.plotting import figure, curdoc
 from bokeh.models import DatetimeTickFormatter, RELATIVE_DATETIME_CONTEXT
 import snscrape_test as sn
 import pandas as pd
-from bokeh.models import Button, TextInput, ColumnDataSource
+from bokeh.models import Button, TextInput, ColumnDataSource, NumericInput
 from bokeh.layouts import column
 import pymongo
+from datetime import datetime, timedelta
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 dblist = myclient.list_database_names()
 mydb = myclient["mydatabase"]
 mycol = mydb["records"]
-from datetime import datetime, timedelta
-
 
 p = figure(height=350, width=1500, x_axis_type="datetime")
-
+source = ColumnDataSource({})
 p.circle(
-    [],
-    [],
+    x="date",
+    y="com_score",
+    source=source,
     size=20,
     color="navy",
     alpha=0.5
 )
-p.line(
-    [],
-    []
-)
+p.line(x="date", y="com_score", source=source)
 
 # Format the axis to show dates properly
 p.xaxis.formatter.context = RELATIVE_DATETIME_CONTEXT()
@@ -45,17 +42,18 @@ def save_records():
     
 def get_records():
     # Remove previous glyphs
-    p.renderers = []
+    # p.renderers = [] ### gammel måde
     # Scrape data
     
-    ###################################################
-    ########## Eksperimentiel kode ####################
-    ###################################################
+
     # Virker pt som den originale kode, men når du fjerne udkommenteringen ved "search_string" giver den fejl
     yesterday = datetime.now() - timedelta(10)  
     search_string = text_input.value# + " since:" + datetime.strftime(yesterday, '%Y-%m-%d') + " until:" + datetime.strftime(yesterday, '%Y-%m-%d')
+    search_string = "data science since:2023-01-01 until:2023-01-05"
+    search_string = text_input.value + " " + since_date.value + " " + until_date.value
     print(search_string)
-    raw_df = sn.sentiment(search_string, 100)
+    global raw_df
+    raw_df = sn.sentiment(search_string, max_entries.value)
     print(raw_df)
     # for i in range(30):
         # yesterday = datetime.now() - timedelta(i)  
@@ -64,40 +62,20 @@ def get_records():
         # temp = sn.sentiment(search_string, 100)
         # pd.concat([raw_df, temp])
         
-    ################################################### 
-    ###################################################
-    ################################################### 
-    
-    #############################################################
-    ######### Udkommenteret original kode: ######################
-    #############################################################
-    # raw_df = sn.sentiment(text_input.value, 100)
-    #############################################################
-    #############################################################
-    #############################################################
-    
-    # Group by date: day. This converts it from a dataframe to a series
-    df = raw_df.groupby(raw_df.date.dt.to_period('D'))['com_score'].mean()
-    # Convert back to a pandas dataframe
-    df = pd.DataFrame({'date':df.index, 'com_score':df.values})
-    
-    print("Input field value:", text_input.value)
-    print("df.size: ", df.size)
-    print("len():", len(df))
-    print(df)
-
-    p.circle(
-        df['date'],
-        df['com_score'],
-        size=20,
-        color="navy",
-        alpha=0.5
-    )
-    p.line(
-        df['date'],
-        df['com_score']
-    )
-    
+    if raw_df.empty == False:
+        # Group by date: day. This converts it from a dataframe to a series
+        df = raw_df.groupby(raw_df.date.dt.to_period('D'))['com_score'].mean()
+        # Convert back to a pandas dataframe
+        df = pd.DataFrame({'date':df.index, 'com_score':df.values})
+        
+        print("Input field value:", text_input.value)
+        print("df.size: ", df.size)
+        print("len():", len(df))
+        print(df)
+        
+        # replace all data in an existing ColumnDataSource
+        source.data = df
+        
 
 # add a button widget and configure with the call back
 save_button = Button(label="Save data")
@@ -106,7 +84,10 @@ save_button.on_event('button_click', save_records)
 search_button = Button(label="Get data")
 search_button.on_event('button_click', get_records)
 
-text_input = TextInput(value="default", title="Label:")
+text_input = TextInput(value="default", title="Search string:")
+max_entries = NumericInput(value=1, low=1, high=10, title="Total tweets to retrieve (1-10):")
+since_date = TextInput(value="since:2023-01-01", title="Since:")
+until_date = TextInput(value="until:2023-01-10", title="Until:")
 
 # put the button and plot in a layout and add to the document
-curdoc().add_root(column(save_button, search_button, text_input, p))
+curdoc().add_root(column(save_button, search_button, text_input, max_entries, since_date, until_date, p))
--- snscrape_test.py
+++ snscrape_test.py
@@ -5,7 +5,7 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 def sentiment(search, max_results):
     # our search term, using syntax for Twitter's Advanced Search
-    # search = '"data science"'
+    # search = "data science since:2023-01-01 until:2023-01-01"
 
     # the scraped tweets, this is a generator
         # uses Twitter’s advanced search - see here https://github.com/igorbrigadir/twitter-advanced-search 
@@ -13,18 +13,22 @@ def sentiment(search, max_results):
 
     # slicing the generator to keep only the first 100 tweets
     sliced_scraped_tweets = itertools.islice(scraped_tweets, max_results)
+    # the values of "sliced_scraped_twetts" are retrieved" can only be trieved once, so saving them in here
+    data = list(sliced_scraped_tweets)
+    if data != []:
+        # convert to a DataFrame and keep only relevant columns
+        df = pd.DataFrame(data)[['date', 'rawContent']]
+        #df = pd.DataFrame(sliced_scraped_tweets)['rawContent']
+        
+        analyzer = SentimentIntensityAnalyzer()
 
-    # convert to a DataFrame and keep only relevant columns
-    df = pd.DataFrame(sliced_scraped_tweets)[['date', 'rawContent']]
-    #df = pd.DataFrame(sliced_scraped_tweets)['rawContent']
-
-    analyzer = SentimentIntensityAnalyzer()
-
-    scores = []
-    for text in df['rawContent']:
-        vs = analyzer.polarity_scores(text)
-        scores.append(vs['compound'])
-     
-    df['com_score'] = scores
-    
-    return df
+        scores = []
+        for text in df['rawContent']:
+            vs = analyzer.polarity_scores(text)
+            scores.append(vs['compound'])
+         
+        df['com_score'] = scores
+        
+        return df
+    else:
+        return pd.DataFrame({})
